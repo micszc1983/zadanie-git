16e11f028dc6009179c564fdbd4fbfecbe77ebf2
Michal <ms@coad.pl>
2017-12-17 12:42:51 +0100

    Add random class

---
+++ snakettest2/RandomClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snakettest2
+{
+    class RandomClass
+    {
+
+        public void WriteText()
+        {
+            Console.WriteLine("Random text");
+        }
+
+    }
+
+
+}
